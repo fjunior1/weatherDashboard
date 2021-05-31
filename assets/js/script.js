ab8cb90e33d8dd250b71d37cd76ddb21a847ec5c
Franklyn Diaz <franklyn_diaz@hotmail.com>
2021-05-31 12:54:19 -0400

    Changes to CSS, HTML and JS Added class to text to fix position and colors, Added functions to set and get data in storage, Updated the forecast areas  in html.

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,26 +4,39 @@ Weather dashboard javascript functions
 
 let myKey = "25bdd42dbbe922a93923b851985199ef";
 let city;
+let cityList = ["orlando", "macon", "la vega", "samana"];
 
 function init() {
     console.log("test test");
 }
 
-init();
-
 function getCitiesList() {
-    
+    debugger;
+   let tmp = localStorage.getItem("cities");
+    if (tmp === undefined) {
+        return "";
+    }
+
+    return tmp;
 }
 
-function setCitiesList(){
+cityList = getCitiesList();
+
+function setCitiesList(cityList) {
+    debugger;
+    localStorage.setItem('cities', JSON.stringify(cityList));
 
 }
+
+setCitiesList(cityList);
+
+
 
 function updateCityInfo(temp, wind, humid, uv) {
-    $("#tempTxt")[0].outerText = "temp testJS";
-    $("#windTxt")[0].outerText =  "wind testJS";
-    $("#humidTxt")[0].outerText = "humid testJS";
-    $("#uvindexTxt")[0].outerText = "UV testJS";
+    $("#tempTxt").text("temp testJS");
+    $("#windTxt").text("wind testJS");
+    $("#humidTxt").text("humid testJS");
+    $("#uvindexTxt").text("UV testJS");
 }
 
 updateCityInfo("", "", "", "");
